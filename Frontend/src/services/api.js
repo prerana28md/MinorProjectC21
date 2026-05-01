be8c7062af460548f4bbb3e2aa8a51dbd20b2b0a
prerana28md <mdprerana155@gmail.com>
2026-05-01 14:22:24 +0530

    Frontend Deploment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://tourism-backend-oxiu.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
